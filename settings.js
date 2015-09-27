3dd268ec2e45e219f5cb92ebebee18c2bf6bc7ee
Yuri Kletsun <ykletsun@lyonscg.com>
2015-09-27 23:09:22 +0300

    redraw points without recreation

--- settings.js
+++ settings.js
@@ -4,17 +4,18 @@
 
 function apply(){
     var size = document.getElementById("size").value;
+    var loaderbg = document.getElementById("loaderbg");
+    loaderbg.setAttribute("r", size*2);
+    loader.options.radius = size;
     var width = document.getElementById("width").value/10;
     var loaders = document.getElementsByClassName("loader");
     for (var l = 0; l < loaders.length; l++) {
+        loaders[l].setAttribute("width", size*5);
+        loaders[l].setAttribute("height", size*5);
         for (var i = 0; i<loaders[l].childNodes.length; i++) {
-            //console.log(loaders[l].childNodes[i].tagName);
-            if(loaders[l].childNodes[i].tagName=="circle") {
-                loaders[l].removeChild(loaders[l].childNodes[i]);
-                i--;
+            if(loaders[l].childNodes[i].tagName=="circle" && loaders[l].childNodes[i].id!="loaderbg") {
+                loaders[l].childNodes[i].setAttribute("r", size/10*width);
             }
         }
     }
-    clearInterval(window.loader.interval);
-    window.loader = new Loader({size: size, width: width});
 }
