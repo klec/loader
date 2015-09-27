643cfa2f942c5872b871c2eb05a66caf959eccdd
Yuri Kletsun <ykletsun@lyonscg.com>
2015-09-27 23:35:08 +0300

    small fix for multiple loaders

--- settings.js
+++ settings.js
@@ -4,8 +4,6 @@
 
 function apply(){
     var size = document.getElementById("size").value;
-    var loaderbg = document.getElementById("loaderbg");
-    loaderbg.setAttribute("r", size*2);
     loader.options.radius = size;
     var width = document.getElementById("width").value/10;
     var loaders = document.getElementsByClassName("loader");
@@ -13,8 +11,12 @@ function apply(){
         loaders[l].setAttribute("width", size*5);
         loaders[l].setAttribute("height", size*5);
         for (var i = 0; i<loaders[l].childNodes.length; i++) {
-            if(loaders[l].childNodes[i].tagName=="circle" && loaders[l].childNodes[i].id!="loaderbg") {
-                loaders[l].childNodes[i].setAttribute("r", size/10*width);
+            if (loaders[l].childNodes[i].tagName == "circle") {
+                if (loaders[l].childNodes[i].id == "loaderbg") {
+                    loaders[l].childNodes[i].setAttribute("r", size * 2);
+                } else {
+                    loaders[l].childNodes[i].setAttribute("r", size / 10 * width);
+                }
             }
         }
     }
