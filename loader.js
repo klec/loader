d7d9e57ccd7527358334384fef114aa7a35fb15b
Yuri Kletsun <ykletsun@lyonscg.com>
2015-09-27 23:34:28 +0300

    move circle creation to separate method

--- loader.js
+++ loader.js
@@ -27,31 +27,34 @@ Loader.prototype = {
     },
 
     moveCircle: function (circle, t) {
-        circle.setAttribute("cx", this.options.radius*2.5 + Math.sin(t * Math.PI / 16) * this.options.radius);
-        circle.setAttribute("cy", this.options.radius*2.5 + Math.cos((t * 1.05) * Math.PI / 16) * this.options.radius);
+        circle.setAttribute("cx", this.options.radius * 2.5 + Math.sin(t * Math.PI / 16) * this.options.radius);
+        circle.setAttribute("cy", this.options.radius * 2.5 + Math.cos((t * 1.05) * Math.PI / 16) * this.options.radius);
+    },
+
+    createCircle: function (alfa, radius, id) {
+        var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        circle.setAttribute("fill", "rgb(" + alfa + ", " + alfa + ", " + alfa + ")");
+        circle.setAttribute("r", radius);
+        circle.setAttribute("id", id);//filter="url(#f1)"
+        circle.setAttribute("filter", "url(#blur1)");
+        return circle;
     },
 
     init: function () {
-        var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        circle.setAttribute("fill", "rgb(0,0,0)");
-        circle.setAttribute("r", this.options.radius*2);
-        circle.setAttribute("id", "loaderbg");
-        circle.setAttribute("cx", "50%");
-        circle.setAttribute("cy", "50%");
-        circle.setAttribute("filter", "url(#blur1)");
 
         var loaders = document.getElementsByClassName("loader");
         for (var l = 0; l < loaders.length; l++) {
-            loaders[l].setAttribute("width", this.options.radius*5);
-            loaders[l].setAttribute("height", this.options.radius*5);
-            loaders[l].appendChild(circle);
+            var bgcircle = this.createCircle(0, this.options.radius * 2, "loaderbg");
+            bgcircle.setAttribute("cx", "50%");
+            bgcircle.setAttribute("cy", "50%");
+            loaders[l].setAttribute("width", this.options.radius * 5);
+            loaders[l].setAttribute("height", this.options.radius * 5);
+            loaders[l].appendChild(bgcircle);
             for (var i = 0; i < this.options.length; i++) {
-                circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
                 var alfa = Math.round(Math.sin((i - 4) / 2.4) * 120 + 120);
-                circle.setAttribute("fill", "rgb(" + alfa + ", " + alfa + ", " + alfa + ")");
-                circle.setAttribute("r", this.options.radius/10*this.options.pointSize);
-                circle.setAttribute("id", "circle" + l + i);//filter="url(#f1)"
-                circle.setAttribute("filter", "url(#blur1)");
+                var id = "circle" + l + i;
+                var size = this.options.radius / 10 * this.options.pointSize;
+                circle = this.createCircle(alfa, size, id);  // var or not var
                 loaders[l].appendChild(circle);
             }
         }
